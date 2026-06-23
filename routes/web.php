ef245395d8f2c7943c4ac4943e8411ffd3660495
hieule210924tb <manu210924@gmail.com>
2026-06-23 09:02:44 +0700

    Cơ bản về Laravel Cache

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test-cache', function () {
+    Cache::put('demo', 'Giá trị cache số 1', 30);
+    $checkDemo = Cache::has('demo');
+
+    $valueCache = Cache::get('demo');
+    return response()->json([
+        'checkDemo' => $checkDemo,
+        'valueCheck' => $valueCache
+    ], 200);
 });
